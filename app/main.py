b5c38c57c64fd79881f45bc94da4be49aa5a5750
myyyhhh <164709705+myyyhhh@users.noreply.github.com>
2025-07-09 11:25:39 +0800

    7.8.1

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
+from fastapi import FastAPI, Request, Form, Depends
 
-from fastapi import FastAPI
+
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, RedirectResponse
+from pydantic import BaseModel
+
 
 
 app = FastAPI()
@@ -10,4 +15,34 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # StaticFiles(directory="static")：指定静态文件存放的目录为static。也就是说，static文件夹中的所有文件都可以通过/static路径来访问。
 # name="static"：为挂载的静态文件设置一个名称，可以方便地引用。
 
+templates = Jinja2Templates(directory="templates")
+# /templates：这是模板文件存放的目录，FastAPI会在这个目录下查找HTML模板文件。
 
+
+# 数据类
+class Templates(BaseModel):
+
+
+    request: Request
+
+
+#图表类
+class ChartImage(BaseModel):
+    image: str
+
+
+@app.get("/", response_class=HTMLResponse)
+async def get_home(request: Request):
+
+
+    # 画图表
+    if True:
+        # 这里可以添加生成图表的逻辑
+        pass
+
+    return templates.TemplateResponse(
+        "index.html", # template/index.html
+        {
+            "request": request
+        },
+    )
